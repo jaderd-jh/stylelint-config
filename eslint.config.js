f52bff99148e5ce5dd29bf9bfaa5d54464e6319e
無患子 <jiangang.zhang@jaderd.com>
2024-05-22 17:14:50 +0800

    chore(deps): update deps

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import jhqn from '@jhqn/eslint-config'
+
+export default jhqn(
+  {
+    lessOpinionated: true,
+    stylistic: {
+      overrides: {
+        'style/indent': 'off',
+        'style/jsx-indent': 'off',
+        'style/jsx-one-expression-per-line': 'off',
+        'style/multiline-ternary': 'off',
+        'style/operator-linebreak': 'off',
+      },
+    },
+    vue: {
+      overrides: {
+        'vue/script-indent': 'off', // https://biomejs.dev/internals/language-support/#html-super-languages-support
+      },
+    },
+  },
+)
